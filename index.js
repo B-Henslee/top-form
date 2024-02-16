e4ac6381e007eb7f03caf59b8723705680c7d248
Toss <126696033+B-Henslee@users.noreply.github.com>
2024-02-16 18:45:01 +0100

    Simplified JS create new class that adds color

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-const password = document.querySelector('#password');
-const passConfirm = document.querySelector('#password_confirm');
-const form = document.querySelector('.fifty');                          //create selector for our form
-const passwordMatch = form.querySelector('.middle-left:nth-child(5)');
-const rightSideMatch = form.querySelector('.middle-right:nth-child(6)');
-
-// Add an input event listener to the password and confirm password fields
-password.addEventListener('input', checkPasswordMatch);
-passConfirm.addEventListener('input', checkPasswordMatch);
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#password_confirm");
+let errorClass = document.querySelector("#passwordDiv")
 
 
-// Create a new div element
-const newDiv = document.createElement('div');
-const blankDiv = document.createElement('div');
-
-
-
-function checkPasswordMatch() {
-    // Compare the values of the password and confirm password fields
-    if (passConfirm.value !== password.value) {
-        newDiv.textContent = "*Passwords don't match!";
-        newDiv.style.color='red';
-        passwordMatch.appendChild(newDiv);
-
-    }else {
-        newDiv.textContent = "";  // Reset the message if passwords match
+function passwordMatch(){
+    if(password.value !== passwordConfirm.value){
+        errorClass.classList.add("error");
+        password.style.borderColor = "red"
+        passwordConfirm.style.borderColor = "red"
+    }else{
+        errorClass.classList.remove("error");
+        password.style.borderColor = ""
+        passwordConfirm.style.borderColor = ""
     }
 }
+
+password.addEventListener("input",passwordMatch)
+passwordConfirm.addEventListener("input",passwordMatch)
+
